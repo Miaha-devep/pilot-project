abaec2f1ef8dbb8924ef35d74492a2d878d24294
MishaOpsDev <mihail.rogachevsky@yandex.ru>
2023-03-29 17:10:30 +0300

    Start game

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def test_func():
-    print("Hello, world")
+import sys
+import pygame
 
 
-if __name__ == "__main__":
-    test_func()
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    while True:
+        for event in pygame.event.get():
+            if event == pygame.QUIT:
+                sys.exit()
+        pygame.display.flip()
+
+
+run_game()
